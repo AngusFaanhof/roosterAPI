3349c9780ac02b8aa844b56ee582e1f26c9dfbaf
AngusFaanhof <angus.faanhof@protonmail.com>
2023-05-18 16:29:03 +0200

    can now convert from csv to api format

--- converter.js
+++ converter.js
@@ -1,22 +1,42 @@
 // require fs
 const fs = require('fs');
 
-const csv = fs.readFileSync('downloads/timetable_2023-05-16.csv', 'utf8');
-const lines = csv.split('\n');
+const csv = fs.readFileSync('downloads/timetable_2023-05-16.csv', 'utf8').split('"').join("");
 
-// group by date
-const grouped = {};
-for (let i = 1; i < lines.length - 1; i++) {
-	const line = lines[i];
-	// console.log(line)
-	const cells = line.split(';');
-	// console.log(typeof cells[4]);
-	const date = cells[4].replace('"', '').replace('"', '');
+function csvToApi(csv) {
+	const lines = csv.split('\n');
+	const apiOutput = {};
 
-	if (!grouped[date])
-		grouped[date] = [];
+	for (let i = 1; i < lines.length - 1; i++) {
+		const cells = lines[i].split(';');
 
-	grouped[date].push(cells);
+		const startDate = cells[4];
+		const startTime = cells[5];
+
+		const endDate = cells[7];
+		const endTime =  cells[8];
+
+		const lineData = {
+			"description": cells[0],
+			"courseCode": cells[1],
+			"start": new Date(`${startDate}T${startTime}Z`),
+			"end": new Date(`${endDate}T${endTime}Z`),
+			"duration": cells[9],
+			"type": cells[10],
+			"teachers": cells[11].split('.,'),
+			"locations": cells[12].split(','),
+			"online": cells[19] == "Ja"
+		}
+
+		if (!apiOutput[startDate])
+			apiOutput[startDate] = [];
+
+		apiOutput[startDate].push(lineData);
+	}
+
+	return apiOutput;
 }
 
-console.log(grouped);
+const data = csvToApi(csv);
+
+// console.log()
